6f1c3799d1f876c0061897e77153aae9aeec6672
Alireza Safaierad <alireza.safaierad@gmail.com>
2026-09-25 17:51:33 +0200

    refactor: migrate from clsx and tailwind-merge to cn for class management

--- src/lib/styles/cn.ts
+++ src/lib/styles/cn.ts
@@ -1,9 +1,6 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import type { ClassValue } from "cn";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
+export { cn } from "cn";
 
 export function join(...classes: ClassValue[]) {
   return classes.filter(Boolean).join(" ");
